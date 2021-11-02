84c490985b308e4c318f1a79a73c0eea65d97e06
Abourass <abourassa@assetval.com>
2021-11-02 14:47:42 -0600

    :art:

--- data/vars.js
+++ data/vars.js
@@ -1,11 +1,15 @@
 module.exports = {
   attackTypes: ['Physical', 'Magical', 'Other'],
-  stats: [
-    {shortTxt: 'Str', fullTxt: 'Strength'},
-    {shortTxt: 'Int', fullTxt: 'Intelligence'},
-    {shortTxt: 'Dex', fullTxt: 'Dexterity'},
-    {shortTxt: 'Con', fullTxt: 'Constitution'},
-    {shortTxt: 'Pow', fullTxt: 'Power'},
-    {shortTxt: 'Luck', fullTxt: 'Luck'},
-  ],
+  stats: {
+    top: [
+      {shortTxt: 'Str', fullTxt: 'Strength'},
+      {shortTxt: 'Int', fullTxt: 'Intelligence'},
+      {shortTxt: 'Dex', fullTxt: 'Dexterity'},
+    ],
+    bottom: [
+      {shortTxt: 'Con', fullTxt: 'Constitution'},
+      {shortTxt: 'Pow', fullTxt: 'Power'},
+      {shortTxt: 'Luck', fullTxt: 'Luck'},
+    ]
+  },
 }
